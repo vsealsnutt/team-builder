3eccd77b7c396350e61529aaea31ac76da4631d0
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-05 11:17:00 -0500

    created a react app, imported the useState hook to keep my team members list, and created a components folder with a form.js file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [values, setValues] = useState({ name: '', email: '', role: '' });
+
+  return (
+    <div>
+      <h1>Team Builder</h1>
+      <Form 
+        values={values}
+      />
+    </div>
+  );
+}
+
+export default App;
