a0847efe28862b848af15a0024a9ea389549c090
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-05 11:59:47 -0500

    defined onChange and onSubmit. Team Builder can now be viewed in the browser

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ function App() {
   const [teamMembers, setTeamMembers] = useState([]);
   const [values, setValues] = useState({ name: '', email: '', role: '' });
 
+  const onChange = (name, value) => {
+    setValues({...values, [name]: value});
+  }
+
+  const onSubmit = () => {
+    setTeamMembers([values, ...teamMembers]);
+  }
+
   return (
     <div>
       <h1>Team Builder</h1>
       <Form 
         values={values}
+        change={onChange}
+        submit={onSubmit}
       />
     </div>
   );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,17 +4,22 @@ const Form = (props) => {
 
     const handleChange = event => {
         const { name, value } = event.target;
-        props.handleChange(name, value);
+        props.change(name, value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.submit();
     }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name
                 <input
                     type='text'
                     name='name'
                     placeholder='Enter your name'
-                    value={values.name}
+                    value={props.values.name}
                     onChange={handleChange}
                 />
             </label>
@@ -23,12 +28,12 @@ const Form = (props) => {
                     type='email'
                     name='email'
                     placeholder='Enter your email'
-                    value={values.email}
+                    value={props.values.email}
                     onChange={handleChange}
                 />
             </label>
             <label>Role
-                <select value={values.role} name='role' onChange={handleChange}>
+                <select value={props.values.role} name='role' onChange={handleChange}>
                     <option value=''>--- Select a Role ---</option>
                     <option value='frontend engineer'>Frontend Engineer</option>
                     <option value='backend engineer'>Backend Engineer</option>
