3a0d2d68083142978a4c24b77bc596996565c583
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-05 11:38:51 -0500

    built out the structure of my form and exported/imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Form from './Components/Form';
 import './App.css';
 
 function App() {
---
+++ src/Components/Form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Form = (props) => {
+
+    return (
+        <form>
+            <label>Name
+                <input
+                    type='text'
+                    name='name'
+                    placeholder='Enter your name'
+                    value={values.name}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Email
+                <input
+                    type='email'
+                    name='email'
+                    placeholder='Enter your email'
+                    value={values.email}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Role
+                <select value={values.role} name='role' onChange={handleChange}>
+                    <option value=''>--- Select a Role ---</option>
+                    <option value='frontend engineer'>Frontend Engineer</option>
+                    <option value='backend engineer'>Backend Engineer</option>
+                    <option value='fullstack engineer'>Full-Stack Engineer</option>
+                    <option value='designer'>UX/UI Designer</option>
+                    <option value='qa engineer'>QA Engineer</option>
+                    <option value='project manager'>Project Manager</option>
+                </select>
+            </label>
+        </form>
+    )
+}
+
+export default Form;
